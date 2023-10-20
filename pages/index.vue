5f5e2e2bd9f6189cebc8f358a1129d8b53b81823
bngrid <bngrid@outlook.com>
2023-10-20 15:24:04 +0800

    兼容更多浏览器，支持多指翻页

--- pages/index.vue
+++ pages/index.vue
@@ -3,19 +3,26 @@
     title: 'Home'
   })
   onMounted(() => {
-    screenWidth = innerWidth
-    addEventListener('resize', () => {
-      screenWidth = innerWidth
-      pageLeft.value = -screenWidth * pageNumber
-    })
-    addEventListener('keyup', keyup)
+    pageWidth = page.value!.offsetWidth
+    onresize = () => {
+      pageWidth = page.value!.offsetWidth
+      pageLeft.value = -pageWidth * pageNumber
+    }
+    onkeyup = (event: KeyboardEvent) => {
+      switch (event.key) {
+        case 'ArrowLeft':
+          if (pageNumber > 0) pageNumber--
+          break
+        case 'ArrowRight':
+          if (pageNumber < store.list.length - 1) pageNumber++
+          break
+      }
+      pageLeft.value = -pageWidth * pageNumber
+    }
   })
   onUnmounted(() => {
-    removeEventListener('resize', () => {
-      screenWidth = innerWidth
-      pageLeft.value = -screenWidth * pageNumber
-    })
-    removeEventListener('keyup', keyup)
+    onresize = null
+    onkeyup = null
   })
   const pageStyle = computed(() => ({
     left: `${pageLeft.value}px`
@@ -35,114 +42,109 @@
   }))
   const store = useStore()
   const colorMode = useColorMode()
+  const page = ref<HTMLElement | null>(null)
   const pageLeft = ref(0)
-  const pointerLeft = computed(() => (-pageLeft.value * 12) / screenWidth)
-  let isPageDown = false
+  const pointerLeft = computed(() => (-pageLeft.value * 12) / pageWidth)
+  let pointerList: {
+    id: number
+    x: number
+  }[] = []
   let isPageDelay = false
-  let pointerNumber = 0
-  let screenWidth = 0
-  let pageNumber = 0
+  let pageWidth = 0
   let initialLeft = 0
+  let pageNumber = 0
   let pageTimer = 0
   const toggleMode = () => {
     colorMode.preference = colorMode.preference === 'dark' ? 'light' : 'dark'
   }
   const pageDown = (event: PointerEvent) => {
-    pointerNumber++
-    isPageDown = true
-    if (pointerNumber === 1) {
+    page.value!.setPointerCapture(event.pointerId)
+    pointerList.push({ id: event.pointerId, x: event.screenX })
+    if (pointerList.length === 1) {
       initialLeft = event.clientX
+      page.value!.onpointermove = pageMove
       pageTimer = +setTimeout(() => {
         isPageDelay = true
       }, 600)
     }
   }
   const pageMove = (event: PointerEvent) => {
-    if (pointerNumber === 1) {
-      pageLeft.value =
-        pageLeft.value +
-        event.movementX /
-          (pageLeft.value > 0 || pageLeft.value < -screenWidth * (store.list.length - 1)
-            ? 3
-            : 1)
-    }
+    pageLeft.value +=
+      (event.screenX - pointerList.find((pointer) => pointer.id === event.pointerId)!.x) /
+      (pageLeft.value > 0 || pageLeft.value < -pageWidth * (store.list.length - 1)
+        ? 3
+        : 1)
+    pointerList.find((pointer) => pointer.id === event.pointerId)!.x = event.screenX
   }
   const pageUp = (event: PointerEvent) => {
-    if (pointerNumber === 1) {
+    page.value!.releasePointerCapture(event.pointerId)
+    if (pointerList.length === 1) {
+      page.value!.onpointermove = null
       const distance = event.clientX - initialLeft
       if (distance) {
         pageNumber = Math.max(
           Math.min(
             isPageDelay
-              ? Math.floor(-pageLeft.value / screenWidth + 0.5)
+              ? Math.floor(-pageLeft.value / pageWidth + 0.5)
               : distance > 0
-              ? pageNumber - Math.ceil(distance / screenWidth)
-              : pageNumber - Math.floor(distance / screenWidth),
+              ? pageNumber - Math.ceil(distance / pageWidth)
+              : pageNumber - Math.floor(distance / pageWidth),
             store.list.length - 1
           ),
           0
         )
-        pageLeft.value = -screenWidth * pageNumber
+        pageLeft.value = -pageWidth * pageNumber
       }
-      isPageDown = false
       isPageDelay = false
       clearTimeout(pageTimer)
     }
-    if (pointerNumber > 0) pointerNumber--
+    pointerList.splice(
+      pointerList.findIndex((pointer) => pointer.id === event.pointerId),
+      1
+    )
   }
-  const pageleave = (event: PointerEvent) => {
-    if (event.pointerType === 'mouse') pageUp(event)
-  }
-  const pageCancel = () => {
-    pointerNumber = 0
-    pageLeft.value = -screenWidth * pageNumber
-    isPageDown = false
+  const pageCancel = (event: PointerEvent) => {
+    page.value!.onpointermove = null
+    pointerList = []
+    pageLeft.value = -pageWidth * pageNumber
     clearTimeout(pageTimer)
     isPageDelay = false
-  }
-  const keyup = (event: KeyboardEvent) => {
-    switch (event.key) {
-      case 'ArrowLeft':
-        if (pageNumber > 0) pageNumber--
-        break
-      case 'ArrowRight':
-        if (pageNumber < store.list.length - 1) pageNumber++
-        break
-    }
-    pageLeft.value = -screenWidth * pageNumber
   }
   const wheel = (event: WheelEvent) => {
     if (event.deltaY > 0 && pageNumber < store.list.length - 1) pageNumber++
     if (event.deltaY < 0 && pageNumber > 0) pageNumber--
-    pageLeft.value = -screenWidth * pageNumber
+    pageLeft.value = -pageWidth * pageNumber
   }
   const toPage = (pageIndex: number) => {
     pageNumber = pageIndex > pageNumber ? pageIndex - 2 : pageIndex - 1
-    pageLeft.value = -screenWidth * pageNumber
+    pageLeft.value = -pageWidth * pageNumber
   }
 </script>
 
 <template>
   <div
-    class="relative h-[100dvh] bg-zinc-100 dark:bg-zinc-800 overflow-hidden touch-none select-none duration-300"
+    class="relative h-full bg-zinc-100 dark:bg-zinc-800 overflow-hidden touch-none select-none duration-300"
+    ref="page"
     @pointerdown.left="pageDown"
-    @pointermove="pageMove"
     @pointerup="pageUp"
-    @pointerleave="pageleave"
     @pointercancel="pageCancel"
     @wheel="wheel"
   >
     <a
       class="absolute top-[20px] right-[20px] w-[24px] h-[24px] bg-white i-charm-github z-10 mix-blend-difference"
       href="https://github.com/bngrid/bngrid"
+      title="访问该项目的开源社区"
+      @pointerdown.stop
     ></a>
     <button
       class="absolute top-[20px] right-[60px] w-[24px] h-[24px] bg-white i-charm-sun dark:i-charm-moon dark:w-[24px] dark:h-[24px] dark:bg-white z-10 mix-blend-difference"
+      :title="`切换到${colorMode.preference === 'dark' ? '浅色模式' : '深色模式'}`"
       @click="toggleMode"
+      @pointerdown.stop
     ></button>
     <div
       class="absolute flex h-full"
-      :class="isPageDown ? 'duration-0' : 'duration-300'"
+      :class="pointerList.length ? 'duration-0' : 'duration-300'"
       :style="pageStyle"
     >
       <div
@@ -161,17 +163,19 @@
       </div>
     </div>
     <div
-      class="flex justify-between absolute inset-x-0 bottom-[30px] mx-auto mix-blend-difference"
+      class="flex justify-between absolute inset-x-0 bottom-[30px] rounded-[1.5px] mx-auto mix-blend-difference overflow-hidden"
       :style="paginationStyle"
     >
       <div
         v-for="pageIndex in store.list.length + 1"
         class="w-[6px] h-[6px] rounded-[1.5px] bg-white opacity-60 cursor-pointer"
+        :title="`跳转到第${pageIndex > pageNumber ? pageIndex - 1 : pageIndex}页`"
         @click="toPage(pageIndex)"
+        @pointerdown.stop
       ></div>
       <div
         class="absolute bg-white w-[18px] h-[6px] rounded-[1.5px]"
-        :class="isPageDown ? 'duration-0' : 'duration-300'"
+        :class="pointerList.length ? 'duration-0' : 'duration-300'"
         :style="pointerStyle"
       ></div>
     </div>
