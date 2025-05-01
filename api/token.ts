e944f68c1ecb9001eb690f9cc521081395afcef7
bngrid <bngrid@outlook.com>
2025-05-01 23:30:46 +0800

    切换jwt库至jose

--- api/token.ts
+++ api/token.ts
@@ -2,7 +2,7 @@
 
 import { User } from '@/generated/prisma'
 import redis from '@/lib/redis'
-import { sign, verify } from 'jsonwebtoken'
+import { importPKCS8, importSPKI, jwtVerify, SignJWT } from 'jose'
 import { nanoid } from 'nanoid'
 import { cookies, headers } from 'next/headers'
 
@@ -16,32 +16,27 @@ type Token = {
   username: string
 }
 
-const { JWT_PRIVATE_KEY: priv, JWT_PUBLIC_KEY: pub } = process.env
-if (!pub || !priv) throw new Error('未找到 JWT_PUBLIC_KEY 或 JWT_PRIVATE_KEY 环境变量')
+const { JWT_PRIVATE_KEY: privPem, JWT_PUBLIC_KEY: pubPem } = process.env
+if (!privPem || !pubPem) {
+  throw new Error('未找到 JWT_PRIVATE_KEY 或 JWT_PUBLIC_KEY 环境变量')
+}
+const privKey = importPKCS8(privPem, 'RS256')
+const pubKey = importSPKI(pubPem, 'RS256')
 
 export async function signToken({ email, role, username }: Omit<Token, 'nonce' | 'time'>) {
   try {
+    const { 0: header, 1: cookie, 2: priv } = await Promise.all([headers(), cookies(), privKey])
     const nonce = nanoid()
     const key = `token:${username}`
     await redis
-      .pipeline()
-      .hset(key, nonce, (await headers()).get('user-agent') ?? '')
+      .multi()
+      .hset(key, nonce, header.get('user-agent') ?? '')
       .expire(key, 3 * 24 * 60 * 60)
       .exec()
-    const payload = {
-      email,
-      nonce,
-      role,
-      time: Date.now(),
-      username
-    }
-    ;(await cookies()).set(
+    const payload = { email, nonce, role, time: Date.now(), username }
+    cookie.set(
       'token',
-      sign(payload, priv ?? '', {
-        algorithm: 'RS256',
-        expiresIn: '3d',
-        issuer: 'banno'
-      }),
+      await new SignJWT(payload).setProtectedHeader({ alg: 'RS256' }).setIssuer('banno').setIssuedAt().setExpirationTime('3d').sign(priv),
       {
         httpOnly: true,
         maxAge: 3 * 24 * 60 * 60,
@@ -57,24 +52,21 @@ export async function signToken({ email, role, username }: Omit<Token, 'nonce' |
 
 export async function verifyToken() {
   try {
-    const token = (await cookies()).get('token')?.value
+    const { 0: header, 1: cookie, 2: pub } = await Promise.all([headers(), cookies(), pubKey])
+    const token = cookie.get('token')?.value
     if (!token) return data(false, 'TOKEN 不存在')
-    const payload = <Token>verify(token, pub ?? '', {
+    const { payload } = (await jwtVerify(token, pub, {
       algorithms: ['RS256'],
       issuer: 'banno'
-    })
+    })) as { payload: Token }
     if (Date.now() - payload.time < 10 * 60 * 1000) return data(true, payload)
     const key = `token:${payload.username}`
-    if ((await redis.hget(key, payload.nonce)) !== (await headers()).get('user-agent')) {
+    if ((await redis.hget(key, payload.nonce)) !== header.get('user-agent')) {
       await redis.hdel(key, payload.nonce)
       return data(false, 'TOKEN 验证失败')
     }
-    return await signToken({
-      email: payload.email,
-      role: payload.role,
-      username: payload.username
-    })
+    return signToken(payload)
   } catch {
-    return data(false, 'TOKEN 验证失败')
+    return data(false, 'token validation failed')
   }
 }
--- middleware.ts
+++ middleware.ts
@@ -1,15 +1,16 @@
-import { verify } from 'jsonwebtoken'
+import { importSPKI, jwtVerify } from 'jose'
 import { NextRequest, NextResponse } from 'next/server'
 
-const { JWT_PUBLIC_KEY: pub } = process.env
-if (!pub) throw new Error('未找到 JWT_PUBLIC_KEY 环境变量')
+const { JWT_PUBLIC_KEY: pubPem } = process.env
+if (!pubPem) throw new Error('未找到 JWT_PUBLIC_KEY 环境变量')
+const pubKey = importSPKI(pubPem, 'RS256')
 
 const paths = ['/login', '/register', '/verify', '/redirect']
 
 export async function middleware(request: NextRequest) {
   try {
     if (paths.includes(request.nextUrl.pathname)) return NextResponse.next()
-    verify(request.cookies.get('token')?.value ?? '', pub ?? '', {
+    await jwtVerify(request.cookies.get('token')?.value ?? '', (await pubKey) ?? '', {
       algorithms: ['RS256'],
       issuer: 'banno'
     })
